c867b98437abf21d40b9f3dfaf42bc716ba9dfec
thanh tung <tungthanh371948@gmail.com>
2024-02-04 00:14:53 +0700

    update 1:17

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.scss';
 import TableUsers from './component/TableUsers';
 import Header from './component/Header';
-
+import Container from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap';
 function App() {
   return (
-    <div className='app-container'>
-      <Header/>
-      <TableUsers/>
-    </div>
+    
+<div className='app-container'>
+          <Header/>
+          <Container>
+            <TableUsers/>
+          </Container>
+  </div>
   );
 }
 
--- src/component/TableUsers.js
+++ src/component/TableUsers.js
@@ -1,5 +1,53 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import { fetchAllUser } from '../services/UserService';
+
 const TableUsers = (props) => {
-    return(<>TableUsers...</>)
+
+    const [listUsers, setListUsers] = useState([]);
+
+    useEffect(()=>{
+        //call apis
+        //dry
+        getUsers()
+    },[])
+
+    const getUsers = async () => {
+        let res = await fetchAllUser(); 
+        if (res && res.data && res.data.data){
+          setListUsers(res.data.data)
+        }
+    }
+    return(<>
+
+{/* email: "george.bluth@reqres.in"
+first_name: "George"
+id: 1
+last_name: "Bluth" */}
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First name</th>
+          <th>Last name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUsers && listUsers.length > 0 && listUsers.map((item,index)=> {
+          return(
+          <tr key ={`users-${index}`}>
+            <td>{item.id}</td>
+            <td>{item.email}</td>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+          </tr>)
+        })}
+        
+      </tbody>
+    </Table>
+    </>)
 }
 
 export default TableUsers; 
